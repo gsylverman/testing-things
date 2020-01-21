99e6663ec4948f3024357a892c7cad289d40a20a
gavril.hasmasan <gavril.hasmasan@pitechnologies.ro>
2020-01-21 17:15:48 +0200

    someComp added

---
+++ src/components/App/SomeComp/SomeComp.tsx
@@ -0,0 +1,15 @@
+import  React from 'react';
+
+export interface SomeCompProps {
+    
+}
+ 
+const SomeComp: React.SFC<SomeCompProps> = () => {
+    return (
+        <div>
+            SomeComp
+        </div>
+    );
+};
+ 
+export default SomeComp;
